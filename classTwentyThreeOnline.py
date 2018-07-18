caa76072353a64d833146a9703dbed78198f7b86
Khan <mkhan@digitallynctech.om>
2018-07-18 09:33:43 +0530

    Git part One

---
+++ classTwentyThreeOnline.py
@@ -0,0 +1,43 @@
+git --> version control 
+--> repository management system
+
+
+100 devs-->server-->code(US) --> www.fb.com --> homepage
+
+100 devs -> single source --> to put all code 
+
+--> drive for code
+
+drive --> repository
+
+2 kinds
+-------
+local repository --> local machine
+remote repository --> server
+
+--> git 
+	--> technology 
+	--> install --> use it 
+	--> files / folders(directories)
+--> remote repository
+	--> git server (own)
+	--> third party services
+		->public repository --> free
+		->private repository --> paid
+			- github
+			- bitbucket
+			- gitlab
+
+--> download git and run (install)
+
+-->install git
+-->create a folder (gitDemoPy)
+-->initialise the local repository -- git init
+
+
+
+
+
+
+
+
