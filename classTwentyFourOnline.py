ee6b7d1e3d7a64f7f3e006efa043f0f7716ae12b
Khan <mkhan@digitallynctech.om>
2018-07-19 09:30:29 +0530

    Git branches

---
+++ classTwentyFourOnline.py
@@ -0,0 +1,74 @@
+# git --> init ,  add , commit , push
+.gitignore 
+	-> hidden file
+	-> skipping push of selected files
+	-> passwords , server details (pwd.txt)
+	-> not push such files to remote 
+	-> init folder
+.gitignore
+<filename>.<ext>
+<filename2>.<ext>
+<filename3>.<ext>
+
+filename3 , filename2 , filename --> will not pushed
+
+100 devs    - fb.com 
+100 systems - 1 app
+
+33 systems --> 1 system/3people --> shifts 
+
+1 system --> 3 devs --> k , h , r
+
+fb.com 
+
+partitions --> branchs 
+
+local --> k , h , r , master
+remote --> k , h , r , master 
+
+branch commands 
+----------------
+create a new --> git checkout -b <branchname>
+delete a branch --> git branch -d <branchname>
+switch --> git checkout <branchname>
+list --> git branch
+
+
+remote link --> https://github.com/junaid579/gitdemopy.git
+
+clone --> download the repository 
+
+git clone <remote link>
+git clone https://github.com/cloudera/hue.git
+
+
+project --> git link --> clone
+--> work project
+
+1 project --> 3 people  -> 30 changes
+
+1 person --> -20 changes
+2 person --> -20 changes
+3 person --> -20 changes
+
+pull all changes from remote into your branch
+
+git pull origin <branchname>
+git add 
+git commit 
+git push
+
+adding 
+------
+git add <filename> --> add single file to staging area 
+git add * 
+git add .  --> all files to staging area
+
+
+
+
+
+
+
+
+
